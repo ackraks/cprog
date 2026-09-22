d75a946379b64127f799e9559130a68c77bf810a
ackraks <142217701+ackraks@users.noreply.github.com>
2026-09-22 10:48:17 +0900

    Add files via upload

---
+++ c prog/xy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("sum:%d\n",x+y);
+    printf("diff:%d\n",x-y);
+    printf("product:%d\n",x*y);
+    printf("quotient:%d\n",x/y);
+    return 0;
+}
+ 
